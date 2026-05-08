33934018f79b00a36bb2a6a64b28ac4f39bde875
Addanki Asish <asish.addanki@gmail.com>
2026-05-08 00:06:17 -0700

    Create 17-LetterCombinationsofaPhoneNumber

---
+++ 17-LetterCombinationsofaPhoneNumber/version/python/17-LetterCombinationsofaPhoneNumber_20260508_000617.py
@@ -0,0 +1,23 @@
+# Last updated: 5/8/2026, 12:06:17 AM
+1class Solution:
+2    def letterCombinations(self, digits: str) -> List[str]:
+3        res=[]
+4        digittochar={
+5            '2':"abc",
+6            '3':"def",
+7            '4':"ghi",
+8            '5':"jkl",
+9            '6':"mno",
+10            '7':"pqrs",
+11            '8':"tuv",
+12            '9':"wxyz"
+13        }
+14        def backtrack(i,currstr):
+15            if i==len(digits):
+16                res.append(currstr[:])
+17                return
+18            for n in digittochar[digits[i]]:
+19                backtrack(i+1,currstr+n)
+20        backtrack(0,"")
+21        return res
+22
