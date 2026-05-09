97c2b286aa6ba6366a5500e630c627410d9893a3
Addanki Asish <asish.addanki@gmail.com>
2026-05-08 20:31:16 -0700

    Create 200-NumberofIslands

---
+++ 200-NumberofIslands/version/python/200-NumberofIslands_20260508_203116.py
@@ -0,0 +1,25 @@
+# Last updated: 5/8/2026, 8:31:16 PM
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        islands = 0
+
+        for r in range(len(grid)):
+            for c in range(len(grid[r])):
+                if grid[r][c] == '1':
+                    self.dfs(grid, r, c)
+                    islands += 1
+        
+        return islands
+                
+    def dfs(self, grid: List[List[str]], r: int, c: int) -> None:
+        grid[r][c] = '0' # mark current index seen
+
+        # check each direction
+        lst = [(r+1, c), (r-1, c), (r, c+1), (r, c-1)]
+
+        for row, col in lst:
+            # check bounds and if next is an island
+            if row >= 0 and col >= 0 and row < len(grid) and col < len(grid[row]) and grid[row][col] == '1':
+                self.dfs(grid, row, col)
+
+        return
