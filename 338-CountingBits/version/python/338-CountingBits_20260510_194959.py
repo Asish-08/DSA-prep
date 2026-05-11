a4595ad7478759e7fb99f836ee7a7c6eddd56e95
Addanki Asish <asish.addanki@gmail.com>
2026-05-10 19:49:59 -0700

    Create 338-CountingBits

---
+++ 338-CountingBits/version/python/338-CountingBits_20260510_194959.py
@@ -0,0 +1,7 @@
+# Last updated: 5/10/2026, 7:49:59 PM
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        ans =[0]
+        for i in range(1,n+1):
+            ans.append(ans[i//2]+i%2)
+        return ans
