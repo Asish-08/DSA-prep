dcd496473e50413a6da64eb31cee0279f23117f8
Addanki Asish <asish.addanki@gmail.com>
2026-05-10 19:47:24 -0700

    Create 338-CountingBits

---
+++ 338-CountingBits/version/python/338-CountingBits_20260510_194724.py
@@ -0,0 +1,14 @@
+# Last updated: 5/10/2026, 7:47:24 PM
+1class Solution:
+2    def countBits(self, n: int) -> List[int]:
+3        ans=[0]*(n+1)
+4        def popcount(i):
+5            count=0
+6            while i!=0:
+7                i=i&(i-1)
+8                count+=1
+9            return count
+10        
+11        for i in range(n+1):
+12            ans[i]=popcount(i)
+13        return ans
