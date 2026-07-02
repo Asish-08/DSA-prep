80ce05d87ec6d58787d13d65c9e233aff1c4375a
Addanki Asish <asish.addanki@gmail.com>
2026-07-01 18:52:24 -0700

    Create 706-DesignHashMap

---
+++ 706-DesignHashMap/version/python/706-DesignHashMap_20260701_185223.py
@@ -0,0 +1,46 @@
+# Last updated: 7/1/2026, 6:52:23 PM
+class ListNode:
+    def __init__(self, key=0, val=0, next=None):
+        self.key = key
+        self.val = val
+        self.next = next
+
+class MyHashMap:
+
+    def __init__(self):
+        self.map = [ListNode() for _ in range(1000)]
+
+    def put(self, key: int, value: int) -> None:
+        curr = self.map[key % 1000]
+
+        while curr.next:
+            if curr.next.key == key:
+                curr.next.val = value
+                return
+            curr = curr.next
+        curr.next = ListNode(key, value)
+
+    def get(self, key: int) -> int:
+        curr = self.map[key % 1000]
+
+        while curr.next:
+            if curr.next.key == key:
+                return curr.next.val
+            curr = curr.next
+        return -1
+
+    def remove(self, key: int) -> None:
+        curr = self.map[key % 1000]
+
+        while curr.next:
+            if curr.next.key == key:
+                curr.next = curr.next.next
+                break
+            curr = curr.next
+
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
