060be4e2962fef483907aba1ebd676ca3db2dca3
Addanki Asish <asish.addanki@gmail.com>
2026-09-23 19:47:03 -0700

    Create 6-ZigzagConversion

---
+++ 6-ZigzagConversion/version/python/6-ZigzagConversion_20260923_194702.py
@@ -0,0 +1,13 @@
+# Last updated: 9/23/2026, 7:47:02 PM
+1class Solution:
+2    def convert(self, s: str, numRows: int) -> str:
+3        if numRows==1:
+4            return s
+5        res=""
+6        for r in range(numRows):
+7            increment=2*(numRows-1)
+8            for i in range(r,len(s),increment):
+9                res+=s[i]
+10                if r>0 and r<numRows-1 and (i+increment-2*r)<len(s):
+11                    res+=s[i+increment-2*r]
+12        return res
